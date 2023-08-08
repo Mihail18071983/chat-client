1a3243f2033a4e01298ce56aa3ec0b376764dd42
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-08-08 11:36:19 +0300

    add validation

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,7 +1,12 @@
-import React, { useState, ChangeEvent, MouseEvent } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, ChangeEvent, FormEvent} from "react";
+import * as Yup from "yup";
 
 import styles from "../styles/Main.module.css";
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string().required("Username is required"),
+  room: Yup.string().required("Room is required"),
+});
 
 const FIELDS = {
   USERNAME: "username",
@@ -14,23 +19,43 @@ export const Main = () => {
     [x: string]: string;
   }>({ [USERNAME]: "", [ROOM]: "" });
 
+  const [errors, setErrors] = useState<{
+    [x: string]: string;
+  }>({
+    [USERNAME]: "",
+    [ROOM]: "",
+  });
+
   const handleChange = ({
     target: { value, name },
   }: ChangeEvent<HTMLInputElement>) => {
     setValues({ ...values, [name]: value });
-    };
+  };
 
-    const handleClick = (e:MouseEvent<HTMLAnchorElement>) => {
-        // const isDisabled = Object.values(values).some(value => !value);
-      const isDisabled = !values.username || !values.room;
-        if (isDisabled)  e.preventDefault() 
-    }
+  const handleClick = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    validationSchema
+      .validate(values, { abortEarly: false }) // Validate the form values
+      .then(() => {
+        // Valid form, navigate to the chat page
+        window.location.href = `/chat?name=${values.username}&room=${values.room}`;
+      })
+      .catch((validationErrors:Yup.ValidationError) => {
+        // Invalid form, update the errors state
+        const newErrors:{ [key: string]: string } = {};
+        (validationErrors.inner as Yup.ValidationError[]).forEach((error) => {
+          if (error.path)
+          newErrors[error.path] = error.message;
+        });
+        setErrors(newErrors);
+      });
+  };
 
   return (
     <div className={styles.wrap}>
       <div className={styles.container}>
         <h1 className={styles.heading}>Join</h1>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleClick}>
           <div className={styles.group}>
             <input
               type="text"
@@ -42,6 +67,7 @@ export const Main = () => {
               required
               onChange={handleChange}
             />
+            {errors[USERNAME] && <div className={styles.error}>{errors[USERNAME]}</div>}
           </div>
           <div className={styles.group}>
             <input
@@ -54,12 +80,13 @@ export const Main = () => {
               required
               onChange={handleChange}
             />
+             {errors[ROOM] && <div className={styles.error}>{errors[ROOM]}</div>}
           </div>
-          <Link onClick={handleClick} className={styles.group} to={`/chat?name=${values[USERNAME]}&room=${values[ROOM]}`}>
-            <button className={styles.button} type="submit">
+         
+            <button  className={styles.button} type="submit">
               Sign In
             </button>
-          </Link>
+      
         </form>
       </div>
     </div>
